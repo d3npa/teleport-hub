5d51001e953dcaa852384074eca8728b9efa7525
kyo <kyo@tilde.pink>
2023-11-25 19:00:17 +0900

    proof of concept

---
+++ src/lib.rs
@@ -0,0 +1,41 @@
+use serde::Deserialize;
+use std::process::Command;
+
+#[derive(Clone, Deserialize)]
+pub struct Exit {
+    pub pf_id: String,
+    pub display_name: String,
+}
+
+impl Exit {
+    pub fn add_host(&self, ip: &str) {
+        let pf_id = &self.pf_id;
+        println!("++ pfctl -t{pf_id} -Tadd {ip}");
+        // Command::new("pfctl")
+        //     .args([&format!("-t{pf_id}"), "-Tadd", ip])
+        //     .status()
+        //     .expect("command failed to run");
+    }
+
+    pub fn delete_host(&self, ip: &str) {
+        let pf_id = &self.pf_id;
+        println!("++ pfctl -t{pf_id} -Tdelete {ip}");
+        // Command::new("pfctl")
+        //     .args([&format!("-t{pf_id}"), "-Tdelete", ip])
+        //     .status()
+        //     .expect("command failed to run");
+    }
+
+    pub fn list_hosts(&self) -> Vec<String> {
+        let pf_id = &self.pf_id;
+        println!("++ pfctl -t{pf_id} -Tshow");
+        vec![]
+        // let stdout = Command::new("pfctl")
+        //     .args([&format!("-t{pf_id}"), "-Tshow"])
+        //     .output()
+        //     .expect("command failed to run")
+        //     .stdout;
+        // let output = String::from_utf8(stdout).unwrap();
+        // output.lines().map(|s| s.trim().to_owned()).collect()
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,92 @@
+use serde::Deserialize;
+use std::{fs, net::SocketAddr};
+use teleport_hub::Exit;
+use thiserror::Error;
+
+use axum::{
+    extract::{ConnectInfo, Path, State},
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
+
+#[derive(Deserialize, Clone)]
+struct Config {
+    exits: Vec<Exit>,
+}
+
+#[derive(Debug, Error)]
+enum Error {
+    #[error("Not found")]
+    NotFound,
+}
+
+fn set_host_exit(config: &Config, exit_id: &str, host_ip: &str) -> Result<(), Error> {
+    match config.exits.iter().find(|e| e.pf_id == exit_id) {
+        Some(e) => {
+            for exit in &config.exits {
+                exit.delete_host(host_ip);
+            }
+            e.add_host(host_ip);
+            Ok(())
+        }
+        None => Err(Error::NotFound),
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let config = {
+        let contents = fs::read_to_string("config.toml").unwrap();
+        toml::from_str(&contents).expect("could not parse config toml")
+    };
+
+    let app = Router::new()
+        .route("/", get(index))
+        .route("/teleport/:destination", get(teleport))
+        .with_state(config);
+
+    axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
+        .serve(app.into_make_service_with_connect_info::<SocketAddr>())
+        .await
+        .unwrap();
+}
+
+async fn index(State(config): State<Config>) -> Html<String> {
+    let mut html = String::new();
+    html.push_str(
+        r"
+<!doctype html>
+<head>
+</head>
+<body>
+    <ul>
+"
+        .trim(),
+    );
+    for exit in &config.exits {
+        html.push_str(&format!(
+            "\n        <li><a href=\"/teleport/{}\">Teleport to {}</a></li>",
+            exit.pf_id, exit.display_name
+        ));
+    }
+    html.push_str(
+        "
+    </ul>
+</body>
+",
+    );
+    html.into()
+}
+
+async fn teleport(
+    State(config): State<Config>,
+    Path(destination): Path<String>,
+    ConnectInfo(client_ip): ConnectInfo<SocketAddr>,
+) -> impl IntoResponse {
+    let client_ip = client_ip.ip();
+    match set_host_exit(&config, &destination, &client_ip.to_string()) {
+        Ok(_) => "ok".to_owned(),
+        Err(e) => format!("{e}"),
+    }
+}
